b6f3636601329918a0dedd21ab2cdcda3a0cc740
Alessio789 <alessio.trentin3@gmail.com>
2020-03-14 15:44:14 +0100

    Added Java Web project structure

---
+++ CarPooling/src/main/java/it/marconivr/CarPooling/CarPoolingApplication.java
@@ -0,0 +1,13 @@
+package it.marconivr.CarPooling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPoolingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPoolingApplication.class, args);
+	}
+
+}
---
+++ CarPooling/src/test/java/it/marconivr/CarPooling/CarPoolingApplicationTests.java
@@ -0,0 +1,13 @@
+package it.marconivr.CarPooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPoolingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
